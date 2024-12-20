0fb825efc0325e9cef055eccfdf173b48980b4a7
Chris Johnson <chris@cjohnson.io>
2024-12-20 13:47:54 -0600

    -Debugger statements working

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
-setup();
-
 const setup = () => {
+  debugger;
   const selectFileInput = document.getElementById("selectFile");
 
-  selectFileInput.onChange = (event) => {
+  window.imageFile = window.imageFile || {};
+
+  selectFileInput.addEventListener("change", (event) => {
     const file = event.target.files[0];
 
+    debugger;
+
     if (file) {
+      debugger;
       const image = new Image();
       const objectURL = URL.createObjectURL(file);
 
       image.src = objectURL;
     }
-  };
+
+    window.imageFile = file;
+    window.imageObj = image;
+
+    logState();
+  });
 };
 
-const runSampler = () => {};
-
-const image = new Image();
-// image.src = 'file:///Users/chrisj/pack.jpeg';
-
-image.onload = () => {
-  console.log("image :>> ", image);
-  console.log("image.width :>> ", image.width);
-  console.log("image.height :>> ", image.height);
+const logState = () => {
+  debugger;
+  console.log("window.imageFile :>> ", window.imageFile);
+  console.log("window.imageObj :>> ", window.imageObj);
 };
 
-image.onerror = (err) => {
-  console.error("Failed to load image, error => ", err);
-};
+// const image = new Image();
+// // image.src = 'file:///Users/chrisj/pack.jpeg';
 
-/* Set up file <input> event handlers */
+// image.onload = () => {
+//   console.log("image :>> ", image);
+//   console.log("image.width :>> ", image.width);
+//   console.log("image.height :>> ", image.height);
+// };
 
-const sampler = () => {
-  runSampler();
-};
+// image.onerror = (err) => {
+//   console.error("Failed to load image, error => ", err);
+// };
+
+setup();
