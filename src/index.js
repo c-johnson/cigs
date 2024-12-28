388fd3ae9348b5f1d8348c62195e1aa2d4af63f9
Chris Johnson <chris@cjohnson.io>
2024-12-28 11:27:19 -0600

    -Get 2D rendering context for <canvas> element

--- src/index.js
+++ src/index.js
@@ -1,51 +1,57 @@
 const setup = () => {
+  /* Grab elements */
   const rasterImageOutput = document.getElementById("raster-image");
   const selectFileInput = document.getElementById("select-file");
+  const canvasElement = document.getElementById("canvas-workstation");
+
+  /* Objects */
   let fileReader = new FileReader();
 
   window.imageFile = window.imageFile || {};
   window.imageObj = window.imageObj || {};
 
+  /* Event listeners */
   selectFileInput.addEventListener("change", (event) => {
     const file = event.target.files[0];
 
     if (file) {
       fileReader.onload = (ev) => {
-        const base64file = ev.target.result;
-        console.log("base64file :>> ", base64file);
+        const base64ImageData = ev.target.result;
+        logState({ base64ImageData });
 
-        rasterImageOutput.src = base64file;
-
-        window.imageFile = file;
-        window.imageObj = base64file;
+        renderCanvasContext({ canvasElement, imageData: base64ImageData });
       };
 
       fileReader.readAsDataURL(file);
     } else {
       console.log("file not a thing");
     }
-
-    logState();
   });
 };
 
-const logState = () => {
+const renderCanvasContext = ({ canvasElement, imageData }) => {
+  const ctx = canvasElement.getContext("2d");
+
+  ctx.fillStyle = "rgb(200 0 0)";
+  ctx.fillRect(10, 10, 50, 50);
+
+  ctx.fillStyle = "rgb(0 0 200 / 50%)";
+  ctx.fillRect(30, 30, 50, 50);
+
+  logState({ ctx });
   debugger;
-  console.log("window.imageFile :>> ", window.imageFile);
-  console.log("window.imageObj :>> ", window.imageObj);
 };
 
-// const image = new Image();
-// // image.src = 'file:///Users/chrisj/pack.jpeg';
+// TODO: Typescript-ify this up later
+//         const vars =  { var1: var1, var2: var2, ... }
+const logState = (vars) => {
+  for (const key in vars) {
+    if (vars.hasOwnProperty(key)) {
+      console.log(key, " :>> ", vars[key]);
+    }
+  }
+};
 
-// image.onload = () => {
-//   console.log("image :>> ", image);
-//   console.log("image.width :>> ", image.width);
-//   console.log("image.height :>> ", image.height);
-// };
-
-// image.onerror = (err) => {
-//   console.error("Failed to load image, error => ", err);
-// };
-
-setup();
+document.addEventListener("DOMContentLoaded", (e) => {
+  setup();
+});
