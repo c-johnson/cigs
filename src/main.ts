9f695e6a6bd4f10c6fac2ce6f79c1d9ac07d89ce
Chris Johnson <chris@cjohnson.io>
2024-12-28 18:09:43 -0600

    -Added some Typescript modules

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
+// Typescript version of main.js
+
 import { getAlgorithms } from "./algorithms";
 
-const main = () => {
+const main = (variable: any) => {
   console.log("getAlgorithms :>> ", getAlgorithms);
+  console.log("variable :>> ", variable);
 };
 
-main();
+main(123);
